f76f2b8dc4a2d612394f24a9cd28a20a9a3b84b9
drkarnat <dineshkarnati100@gmail.com>
2024-08-11 17:59:51 -0400

    Made it so that it gives me the optimized list

--- platepal/backend/scheduler.py
+++ platepal/backend/scheduler.py
@@ -4,8 +4,9 @@ import pandas as pd
 import certifi
 
 calorieConstraint = 3000
-sodiumConstraint = 1000  # in mg
+sodiumConstraint = 2000  # in mg
 proteinConstraint = 180  # in g
+carbohydrateConstraint = 150  # in g
 
 connection_string = "mongodb+srv://dineshkarnati100:agre2u9tQ7v2V1XL@cluster0.nq5d7.mongodb.net/?tls=true"
 
@@ -16,51 +17,96 @@ try:
 except Exception as e:
     print("Error:", e)
 
-
 db = client["diningMenus"]
 collection = db["menus"]
 
 data = pd.DataFrame(list(collection.find()))
 
-
-# I'm literally going to store the menu_items dicts in here
-# calroies, sodium, protei
+# Store the menu_items dicts in foodVariables
 foodVariables = []
 
-# x1 + x2 + x3 + x4
-
-items = data["items"]
-for item_id in items:
-    item = db.items.find_one({"_id": item_id})
-    foodVariables.append(item)
-
-print(foodVariables)
+for menu in data["items"]:
+    for _id in menu:
+        item = db.items.find_one({"_id": _id})
+        foodVariables.append(item)
 
 MY_PROBLEM = LpProblem("platepal", LpMaximize)
 
+food_item_vars = {
+    item["name"]: LpVariable(item["name"], lowBound=0, cat="Binary")
+    for item in foodVariables
+}
+
 MY_PROBLEM += (
-    lpSum(foodVariables["nutrients"][2])
-    + lpSum(foodVariables["nutrients"][8])
-    + lpSum(foodVariables["nutrients"][1])
+    lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][2]["value"])
+            for item in foodVariables
+        ]
+    )
+    + lpSum(
+        [
+            food_item_vars[item["name"]]
+            * float(item["nutrients"][len(item["nutrients"]) - 1]["value"])
+            for item in foodVariables
+        ]
+    )
+    + lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][1]["value"])
+            for item in foodVariables
+        ]
+    )
+    + lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][3]["value"])
+            for item in foodVariables
+        ]
+    )
 )
 
 MY_PROBLEM += (
-    lpSum([food["nutrients"][2] for food in foodVariables]) <= proteinConstraint
+    lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][2]["value"])
+            for item in foodVariables
+        ]
+    )
+    <= proteinConstraint
 )
 MY_PROBLEM += (
-    lpSum([food["nutrients"][8] for food in foodVariables]) <= sodiumConstraint
+    lpSum(
+        [
+            food_item_vars[item["name"]]
+            * float(item["nutrients"][len(item["nutrients"]) - 1]["value"])
+            for item in foodVariables
+        ]
+    )
+    <= sodiumConstraint
 )
 MY_PROBLEM += (
-    lpSum([food["nutrients"][1] for food in foodVariables]) <= calorieConstraint
+    lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][1]["value"])
+            for item in foodVariables
+        ]
+    )
+    <= calorieConstraint
 )
-
-# The variables should be teh food items
-# Goal is to optimize the number of food items so the optimization problem is to max them
+MY_PROBLEM += (
+    lpSum(
+        [
+            food_item_vars[item["name"]] * float(item["nutrients"][3]["value"])
+            for item in foodVariables
+        ]
+    )
+    <= carbohydrateConstraint
+)
 
 solution = MY_PROBLEM.solve()
 
-print(
-    str(pulp.LpStatus[solution])
-    + " ; max value = "
-    + str(pulp.value(MY_PROBLEM.objective))
-)
+print(f"Solution Status: {LpStatus[solution]}")
+
+for item in MY_PROBLEM.variables():
+    if item.varValue == 1:
+        print(item.name)
