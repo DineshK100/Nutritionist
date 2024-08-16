3a8405c6dc605f1e173d1e5f4e172cf5c5be40cb
drkarnat <dineshkarnati100@gmail.com>
2024-08-16 16:41:48 -0400

    Created NNM

--- platepal/backend/optimizerModel.py
+++ platepal/backend/optimizerModel.py
@@ -1,6 +1,49 @@
+import tensorflow as tf
 import numpy as np
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.optimizers import Adam
-from tensorflow.python.keras.layers import Flatten, Dense
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.engine import data_adapter
+import recommender
+from recommender import initialRecommendations
+
+foodItemVector, similarities, itemList, recommended_list = initialRecommendations()
+
+
+# Manually changed this as per a few StackOverflow similar issues with distributed data set specs
+def _is_distributed_dataset(ds):
+    return isinstance(ds, data_adapter.input_lib.DistributedDatasetSpec)
+
+
+data_adapter._is_distributed_dataset = _is_distributed_dataset
 
 # Watching videos understaind neural networks
+
+# inputs -> hidden layers -> outputs
+
+# activation functions
+model = Sequential()
+model.add(Dense(32, activation="relu"))
+model.add(Dense(16, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+adam = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+)
+
+# Temporary simulated user feedback
+feedback_labels = np.random.randint(0, 2, size=(len(similarities)))
+
+training_data = np.hstack((foodItemVector, similarities.reshape(-1, 1)))
+
+model.fit(training_data, feedback_labels, epochs=10, batch_size=8)
+
+predicted_scores = model.predict(training_data).flatten()
+
+new_recs = np.argsort(predicted_scores)[::-1][:10]
+
+recommended_items = [itemList[i] for i in new_recs]
+
+print("Recommended items:", [item["name"] for item in recommended_list])
--- platepal/backend/recommender.py
+++ platepal/backend/recommender.py
@@ -3,41 +3,46 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from connectDb import connectToMongo
 
-db, collection = connectToMongo()
 
-data = pd.DataFrame(list(collection.find()))
+def initialRecommendations():
+    db, collection = connectToMongo()
 
-userPreferences = np.array([500, 50, 1000])  # cals, protein, sodium
+    data = pd.DataFrame(list(collection.find()))
 
-foodItemVector = []
-itemList = []
+    userPreferences = np.array([500, 50, 1000])  # cals, protein, sodium
 
-for menu in data["items"]:
-    for _id in menu:
-        item = db.items.find_one({"_id": _id})
+    foodItemVector = []
+    itemList = []
 
-        if (
-            item
-            # Below statement is done to make 100% sure that only the user preferences are being recommended
-            and float(item["nutrients"][len(item["nutrients"]) - 1]["value"]) <= 1500
-        ):
-            vectorElement = [
-                float(item["nutrients"][1]["value"]),
-                float(item["nutrients"][2]["value"]),
-                float(item["nutrients"][len(item["nutrients"]) - 1]["value"]),
-            ]
-            foodItemVector.append(vectorElement)
-            itemList.append(item)
+    for menu in data["items"]:
+        for _id in menu:
+            item = db.items.find_one({"_id": _id})
 
-# Converted userPreferences array to 2d array to compare as "vectors"
-similarities = cosine_similarity(foodItemVector, [userPreferences])
+            if (
+                item
+                # Below statement is done to make 100% sure that only the user preferences are being recommended
+                and float(item["nutrients"][len(item["nutrients"]) - 1]["value"])
+                <= 1500
+            ):
+                vectorElement = [
+                    float(item["nutrients"][1]["value"]),
+                    float(item["nutrients"][2]["value"]),
+                    float(item["nutrients"][len(item["nutrients"]) - 1]["value"]),
+                ]
+                foodItemVector.append(vectorElement)
+                itemList.append(item)
 
-# since similiarities is an np array I made use of np functions
-# using argsort because it returns the indices of the elements
-# reversing the list because argsort inately goes smallest to largest
-# Get indices of top 10 similarities
-recommended_indices = np.argsort(similarities.flatten())[::-1][:10]
+    # Converted userPreferences array to 2d array to compare as "vectors"
+    similarities = cosine_similarity(foodItemVector, [userPreferences])
 
-recommended_list = [itemList[i] for i in recommended_indices]
+    # since similiarities is an np array I made use of np functions
+    # using argsort because it returns the indices of the elements
+    # reversing the list because argsort inately goes smallest to largest
+    # Get indices of top 10 similarities
+    recommended_indices = np.argsort(similarities.flatten())[::-1][:10]
 
-print([item["name"] for item in recommended_list])
+    recommended_list = [itemList[i] for i in recommended_indices]
+
+    print([item["name"] for item in recommended_list])
+
+    return (foodItemVector, similarities, itemList, recommended_list)
