8c016559004d4b454d8d324a9c4855daeb7ab2c4
drkarnat <dineshkarnati100@gmail.com>
2024-08-15 21:30:47 -0400

    Small commit to start neural network understanding

---
+++ platepal/backend/optimizerModel.py
@@ -0,0 +1,6 @@
+import numpy as np
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.layers import Flatten, Dense
+
+# Watching videos understaind neural networks
